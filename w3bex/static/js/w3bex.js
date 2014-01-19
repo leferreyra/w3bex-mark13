86c8ce1ebf4ac37c3cf226cf9bc12d64e4930f17
Leonardo Ferreyra <leferreyra@gmail.com>
2014-01-19 09:34:49 -0300

    Adds section 'who we are' and it's images

--- w3bex/static/js/w3bex.js
+++ w3bex/static/js/w3bex.js
@@ -1,26 +1,19 @@
-KEY_UP = 38;
-KEY_DOWN = 40;
 
 var w3bex = {}
 
 w3bex.init = function(){
 
-	// Main Site Elements
-	w3bex.main_window = $("#main-window");
+	this.slider = new w3bexSlider({
+		interval: 3,
+		sliderId: "home"
+	});
 
-	w3bex.setMainWindowHeight();
-	window.onresize = w3bex.setMainWindowHeight;
+	this.resizeFirstSection();
+	this.slider.play();
 }
 
-
-w3bex.setMainWindowHeight = function(){
-
-	w3bex.window_height = window.innerHeight;
-	w3bex.header_height = $('header').height()
-	w3bex.main_window_height = w3bex.window_height - w3bex.header_height;
-
-	w3bex.main_window.height(w3bex.main_window_height);
-	w3bex.main_window.find('.inner-window').height(w3bex.main_window_height);
+w3bex.resizeFirstSection = function(){
+	$('section.slider').css('height', window.innerHeight - 51);
 }
 
 $(document).ready(function(){
